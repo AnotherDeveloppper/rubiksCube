383a1afcd15884272a5602a19a32fa210c805c67
salah <salahedine.bourbab@gmail.com>
2022-10-02 13:59:26 +0100

    coments and temp variables removal

--- rubiksCubeInit.py
+++ rubiksCubeInit.py
@@ -11,13 +11,13 @@
 """
 
 
-# colors of rubik cube : 
 
 from genericpath import exists
 from operator import concat
 from turtle import color
 
 
+# colors of rubik cube : ( to be changed)
 colors = ["White","Blue","Red","Yellow","Green","Orange","White","Blue","Red","Yellow","Green","Orange","White","Blue","Red","Yellow","Green","Orange","White","Blue","Red","Yellow","Green","Orange",]
 
 
@@ -41,37 +41,30 @@ class piece :
         print(self.name+" with position: "+ str(self.pos)+" and color: "+' '.join(self.color))
     
 
-# compare lists 
-def compareList(l1,l2):
-    l1.sort()
-    l2.sort()
-    if(l1==l2):
-        return 1
-    else:
-        return 0
-
-y=0
-faceloop=0
-facecounter=0
-facecounter2=0
-temp=0
-pieces = []
-colorTemp=[]
-
-shape =""
+ # needed variables for loop
+y=0 #current face color
+facecounter=0   #2 colors pieces counter for each face
+facecounter2=0  #3 colors pieces counter for each face
+temp=0 # additional value on color to avoid adding 2 opposite face colors together
+pieces = [] # list of all our pieces
+colorTemp=[]    # temporary colors of each piece
 
 # init all pieces 
 
+
+# 1 color piece takes current color ( using variable y)
+# 2 colors piece takes current color + 1  of the rest 4 colors that can match ( ex : white with all except yellow)
+# 2 colors piece takes current color + 2  of the rest 4 colors that can match ( ex : white with all except yellow)
+
 for x in range (0,54) : 
     
+
     if x==4 or (x-4)%9==0:
         # 1 color
         colorTemp.append(colors[y])
-        shape+="a"  
 
     if x== 1 or x==3 or x==5 or x==7 or (x-1)%9==0 or (x-3)%9==0 or (x-5)%9==0 or (x-7)%9==0:
         # 2 colors
-        shape+="b"  
         facecounter+=1
         if facecounter>= 3 :
             temp = facecounter+1
@@ -83,14 +76,11 @@ for x in range (0,54) :
         temp =0
     if x== 0 or x==2 or x==6 or x==8 or x%9==0 or (x-2)%9==0 or (x-6)%9==0 or (x-8)%9==0:
         # 3 colors
-        shape+="c"  
         facecounter2+=1
         if facecounter2>=2 :
             temp = facecounter2 +1
             colorTemp.append(colors[y])
             colorTemp.append(colors[y+temp])
-            print(concat("y=",str(y)))
-            print(concat("temp=",str(temp+2+1)))
             if temp+2 ==6 :
                 colorTemp.append(colors[y+temp+2+1])
             else:
@@ -101,12 +91,11 @@ for x in range (0,54) :
             colorTemp.append(colors[y+2])
         temp =0
     
-    faceloop+=1
     if x%9==0 and x!=0:
         y+=1
         facecounter=0
         facecounter2=0
-    #colorTemp.sort()
+    colorTemp.sort()
     
     name = 'piece_{}'.format(x) 
     exists = False
@@ -120,8 +109,8 @@ for x in range (0,54) :
     colorTemp=[]
     exists= False
 
-## error 1 piece : multiple positions 
 
+# show all pieces
 for x in pieces: 
     x.toString()
 
