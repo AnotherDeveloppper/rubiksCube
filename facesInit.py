e53d2436f2c5cb6c6c171c95527201f6991ba03b
salah <salahedine.bourbab@gmail.com>
2022-10-06 00:37:26 +0100

    save ( might delete)

---
+++ facesInit.py
@@ -0,0 +1,33 @@
+"""
+            UUU                                 0  1  2                                                     O O O
+            UUU                                 3  4  5                                                     O O O
+            UUU                                 6  7  8                                                     O O O
+        LLL FFF RRR BBB               9  10 11  18 19 20    27 28 29    36 37 38                   G G G    W W W    B B B   Y Y Y
+        LLL FFF RRR BBB               12 13 14  21 22 23    30 31 32    39 40 41                   G G G    W W W    B B B   Y Y Y
+        LLL FFF RRR BBB               15 16 17  24 25 26    33 34 35    42 43 44                   G G G    W W W    B B B   Y Y Y
+            DDD                                 45 46 47                                                    R R R
+            DDD                                 48 49 50                                                    R R R
+            DDD                                 51 52 53                                                    R R R
+"""
+
+
+import rubiksCubeInit 
+from rubiksCubeInit import piece
+from rubiksCubeInit import pieces
+
+positions = ["U","L","F","R","B","D"]
+faces = []
+class face : 
+    pos = []
+    name = ""
+    def __init__(self,name, pos) -> None:
+        self.pos=pos
+        self.name=name
+    
+
+counter = 0
+
+for x in range(0,26):
+
+    if(counter)
+    faces.append()
